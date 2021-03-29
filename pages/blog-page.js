aa221cf6a40a0c2dca3799def27ec83245a16cd6
15951595 <shun19871001@gmail.com>
2021-03-29 21:47:37 +0900

    contact

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,11 +1,33 @@
 import Layout from "./components/Layout"
+import { getAllPostsData } from "../lib/posts";
+import Post from "./components/posts";
 
-const Blog = () => {
+const Blog = ( { posts } ) => {
   return (
     <Layout title='blog'>
-      blog
+      <ui className="m-10">
+        {posts && posts.map((post) => {
+          // <Post key={post.id} post={post} />
+          <p>post</p>
+        })}
+      </ui>
     </Layout>
   )
+}
+
+// export const getStaticProps = async () => {
+//   const posts = await getAllPostsData();
+//   return {
+//     props: { posts }
+//   }
+// }
+
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  console.log(posts)
+  return {
+    props: { posts },
+  };
 }
 
 export default Blog
