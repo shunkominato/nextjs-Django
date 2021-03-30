59c6080208e877e0e051a5f265800ddef8a57d52
15951595 <shun19871001@gmail.com>
2021-03-30 21:07:12 +0900

    vercel deploy

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
     <Layout title='aa'>
-      <p className="text-4xl">helo</p>
+      <p className="text-4xl">helo ww</p>
     </Layout>
   )
 }
